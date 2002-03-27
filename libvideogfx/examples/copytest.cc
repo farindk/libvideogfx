6569c6bc817bfbd7ed128ba1063477ba54dab542
farin <farin@89ed393f-2632-0410-9d69-91a09fb2310b>
2002-03-27 13:59:45 +0000

    new test program for scaled copy routine
    git-svn-id: svn://svn.code.sf.net/p/libvideogfx/code/trunk@13 89ed393f-2632-0410-9d69-91a09fb2310b

---
+++ libvideogfx/examples/copytest.cc
@@ -0,0 +1,60 @@
+
+#include "libvideogfx/x11/imgwin.hh"
+#include "libvideogfx/graphics/fileio/jpeg.hh"
+#include "libvideogfx/graphics/draw/blit.hh"
+#include "libvideogfx/graphics/draw/format.hh"
+
+#include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
+
+
+// ARGS: give me 4 JPEG files
+
+main(int argc,char** argv)
+{
+  srand(time(NULL));
+
+  Image<Pixel> img[5];
+  for (int i=0;i<4;i++)
+    {
+      ReadImage_JPEG(img[i], argv[i+1]);
+    }
+
+  ImageParam spec = img[0].AskParam();
+  img[4].Create(spec);
+
+  ImageWindow_Autorefresh_X11 win;
+  win.Create(spec.width,spec.height,"test");
+
+  int w = spec.width;
+  int h = spec.height;
+
+#if 0
+  Copy(img[4],0,0,
+       img[0],0,0,w/2,h/2);
+  Copy(img[4],w/2,0,
+       img[1],w/2,0,w/2,h/2);
+  Copy(img[4],0,h/2,
+       img[2],0,h/2,w/2,h/2);
+  Copy(img[4],w/2,h/2,
+       img[3],w/2,h/2,w/2,h/2);
+#else
+  CopyScaled(img[4],0,0,w/2,h/2,
+	     img[0],0,0,w,h);
+  CopyScaled(img[4],w/2,0,w/2,h/2,
+	     img[1],0,0,w,h);
+  CopyScaled(img[4],0,h/2,w/2,h/2,
+	     img[2],0,0,w,h);
+  CopyScaled(img[4],w/2,h/2,w/2,h/2,
+	     img[3],0,0,w,h);
+#endif
+
+  for (int i=0;i<5;i++)
+    {
+      win.Display(img[i]);
+      sleep(1);
+    }
+
+  sleep(10);
+}
