0576aa5c80d341de027d3dc363f0a466bb45ddde
farin <farin@89ed393f-2632-0410-9d69-91a09fb2310b>
2002-04-05 17:29:08 +0000

    - new ImageSink and ImageSink_Save classes
    git-svn-id: svn://svn.code.sf.net/p/libvideogfx/code/trunk@25 89ed393f-2632-0410-9d69-91a09fb2310b

---
+++ libvideogfx/libvideogfx/graphics/fileio/imagesink.hh
@@ -0,0 +1,63 @@
+/*********************************************************************
+  fileio/imagesink.hh
+
+  purpose:
+    Abstract image sink. Just throw in any kind of images and they
+    will be displayed/stored/...
+
+  notes:
+
+  to do:
+
+  author(s):
+   - Dirk Farin, dirk.farin@gmx.de
+
+  modifications:
+   05/Apr/2002 - Dirk Farin - first implementation
+ *********************************************************************/
+
+#ifndef LIBVIDEOGFX_GRAPHICS_FILEIO_IMAGESINK_HH
+#define LIBVIDEOGFX_GRAPHICS_FILEIO_IMGAESINK_HH
+
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+#include <libvideogfx/graphics/basic/image.hh>
+
+
+class ImageSink
+{
+public:
+  virtual ~ImageSink() { }
+
+  virtual void SendImage(const Image<Pixel>&) = 0;
+};
+
+
+
+enum ImageFileFormat
+  {
+    Format_JPEG, Format_PPM, Format_PGM // , Format_PlanarYUV420
+  };
+
+class ImageSink_Save : public ImageSink
+{
+public:
+  ImageSink_Save(ImageFileFormat fmt = Format_JPEG);
+  ~ImageSink_Save();
+
+  void SetFileFormat(ImageFileFormat fmt) { d_format = fmt; }
+  void SetFilename(const char*,bool autosuffix=false);
+  void SetCurrentImageNr(int n) { d_curr_nr = n; }
+
+  void SendImage(const Image<Pixel>&);
+
+private:
+  ImageFileFormat d_format;
+  char* d_filename_template;
+  bool  d_autosuffix;
+  int   d_curr_nr;
+};
+
+#endif
