48dad3ab9069315b7d8639817b22cf23fd1663e9
farin <farin@89ed393f-2632-0410-9d69-91a09fb2310b>
2002-05-01 22:45:51 +0000

    made cputest.cc fault-proof
    git-svn-id: svn://svn.code.sf.net/p/libvideogfx/code/trunk@33 89ed393f-2632-0410-9d69-91a09fb2310b

--- libvideogfx/examples/cputest.cc
+++ libvideogfx/examples/cputest.cc
@@ -9,18 +9,29 @@ using namespace videogfx;
 
 main(int argc,char** argv)
 {
-  char buf[1000+1];
-  CPU_Capabilities* cpu = CPU_Capabilities::AskCapabilities();
+  try
+    {
+      char buf[1000+1];
+      CPU_Capabilities* cpu = CPU_Capabilities::AskCapabilities();
 
-  cpu->GetCPUInfo(buf,1000);
+      cpu->GetCPUInfo(buf,1000);
 
-  cout << "CPU    : " << buf << endl;
-  cout << "Arch   : " << cpu->GetArchitectureName() << endl;
+      cout << "CPU    : " << buf << endl;
+      cout << "Arch   : " << cpu->GetArchitectureName() << endl;
 
-  cout << "MMX    : " << (cpu->HasMMX() ? "yes" : "no") << endl;
-  cout << "MMX-ext: " << (cpu->HasMMX2() ? "yes" : "no") << endl;
-  cout << "3dNow! : " << (cpu->Has3dNow() ? "yes" : "no") << endl;
-  cout << "MTRR   : " << (cpu->HasMTRR() ? "yes" : "no") << endl;
-  cout << "CMOV   : " << (cpu->HasCMOV() ? "yes" : "no") << endl;
-  cout << "FPU    : " << (cpu->HasFPU() ? "yes" : "no") << endl;
+      if (cpu->GetArchitecture() == CPUArch_X86)
+	{
+	  cout << "MMX    : " << (cpu->HasMMX() ? "yes" : "no") << endl;
+	  cout << "MMX-ext: " << (cpu->HasMMX2() ? "yes" : "no") << endl;
+	  cout << "3dNow! : " << (cpu->Has3dNow() ? "yes" : "no") << endl;
+	  cout << "MTRR   : " << (cpu->HasMTRR() ? "yes" : "no") << endl;
+	  cout << "CMOV   : " << (cpu->HasCMOV() ? "yes" : "no") << endl;
+	  cout << "FPU    : " << (cpu->HasFPU() ? "yes" : "no") << endl;
+	}
+    }
+  catch(const Excpt_Base& e)
+    {
+      MessageDisplay::Show(e);
+    }
 }
+
