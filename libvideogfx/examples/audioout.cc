bfa0bbb3107e8fcec52365b30716429567a894be
farin <farin@89ed393f-2632-0410-9d69-91a09fb2310b>
2002-05-23 17:51:57 +0000

    - new test program for audio output
    git-svn-id: svn://svn.code.sf.net/p/libvideogfx/code/trunk@52 89ed393f-2632-0410-9d69-91a09fb2310b

---
+++ libvideogfx/examples/audioout.cc
@@ -0,0 +1,39 @@
+
+#include <math.h>
+#include <iostream>
+#include <libvideogfx.hh>
+#include <unistd.h>
+
+using namespace std;
+using namespace videogfx;
+
+
+int main()
+{
+  int16 sine[44100];
+
+  for (int i=0;i<44100;i++)
+    {
+      sine[i] = 20000*sin(2*M_PI*i*1000/44100);
+      //cout << i << " " << sine[i] << endl;
+    }
+
+  AudioSink_LinuxSndCard snd;
+
+  for (int i=0;i<10;i++)
+    {
+      int total=44100;
+      while (total)
+	{
+	  int n=snd.SendSamples(&sine[44100-total],&sine[44100-total],total);
+	  if (n)
+	    {
+	      cout << total << " " << n << endl;
+	      total -= n;
+	    }
+	  usleep(100000);
+	}
+    }
+
+  return 0;
+}
