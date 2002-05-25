7e204fd1bfb36f44023ed3cd194b433ec86eb50b
farin <farin@89ed393f-2632-0410-9d69-91a09fb2310b>
2002-05-25 22:28:10 +0000

    smartpointer example
    git-svn-id: svn://svn.code.sf.net/p/libvideogfx/code/trunk@53 89ed393f-2632-0410-9d69-91a09fb2310b

--- libvideogfx/examples/smartpointer.cc
+++ libvideogfx/examples/smartpointer.cc
@@ -1,1 +1,41 @@
-int main() { return 0; }
+
+#include "libvideogfx/libvideogfx.hh"
+
+#include <iostream>
+using namespace std;
+using namespace videogfx;
+
+class test
+{
+public:
+  test(int v) { blubber=v; }
+
+  int blubber;
+};
+
+int main()
+{
+  try
+    {
+      SP<test> v1 = new test(4711);
+      SP<test> v2;
+      v2 = v1;
+      v2 = new test(123);
+
+      v2().blubber = 321;
+
+      cout << v1().blubber << endl;
+      cout << v2().blubber << endl;
+
+      swap(v1,v2);
+
+      cout << v1().blubber << endl;
+      cout << v2().blubber << endl;
+    }
+  catch(const Excpt_Base& e)
+    {
+      MessageDisplay::Show(e);
+    }
+
+  return 0;
+}
