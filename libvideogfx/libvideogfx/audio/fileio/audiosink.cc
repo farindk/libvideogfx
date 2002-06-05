cb56ca60393fc6542bccc5a1084e59ff21b24643
farin <farin@89ed393f-2632-0410-9d69-91a09fb2310b>
2002-06-05 18:53:10 +0000

    support for 8bit and 32bit samples
    git-svn-id: svn://svn.code.sf.net/p/libvideogfx/code/trunk@60 89ed393f-2632-0410-9d69-91a09fb2310b

---
+++ libvideogfx/libvideogfx/audio/fileio/audiosink.cc
@@ -0,0 +1,55 @@
+
+#include "libvideogfx/audio/fileio/audiosink.hh"
+#include "libvideogfx/error.hh"
+#include <algorithm>
+
+namespace videogfx {
+
+  void AudioSink::SendSamples(const int8*  samples,int len,int64 timestamp)
+  {
+    int16 buf[1000];
+
+    while (len)
+      {
+	int l = std::min(len,1000);
+	for (int i=0;i<l;i++)
+	  buf[i] = samples[i] << 8;
+
+	SendSamples(buf,l,timestamp);
+	len -= l;
+      }
+  }
+
+  void AudioSink::SendSamples(const int16* samples,int len,int64 timestamp)
+  {
+    MessageDisplay::Show(ErrSev_Warning,"No 16 bit audio output defined. Most probably, this is an error.");
+
+    int32 buf[1000];
+
+    while (len)
+      {
+	int l = std::min(len,1000);
+	for (int i=0;i<l;i++)
+	  buf[i] = samples[i] << 16;
+
+	SendSamples(buf,l,timestamp);
+	len -= l;
+      }
+  }
+
+  void AudioSink::SendSamples(const int32* samples,int len,int64 timestamp)
+  {
+    int16 buf[1000];
+
+    while (len)
+      {
+	int l = std::min(len,1000);
+	for (int i=0;i<l;i++)
+	  buf[i] = samples[i] >> 16;
+
+	SendSamples(buf,l,timestamp);
+	len -= l;
+      }
+  }
+
+}
