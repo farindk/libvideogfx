1e872cb357fedb669db940c1fdbb2034b9f7a183
farin <farin@89ed393f-2632-0410-9d69-91a09fb2310b>
2002-11-22 13:12:02 +0000

    test routine for dynamic arrays
    git-svn-id: svn://svn.code.sf.net/p/libvideogfx/code/trunk@88 89ed393f-2632-0410-9d69-91a09fb2310b

---
+++ libvideogfx/examples/dynarray.cc
@@ -0,0 +1,34 @@
+
+#include "libvideogfx/libvideogfx.hh"
+#include <iostream>
+
+using namespace videogfx;
+using namespace std;
+
+
+void Show(const DynArray<int>& d)
+{
+  for (int i=0;i<d.AskSize();i++)
+    cout << d[i] << ' ';
+  cout << endl;
+}
+
+main()
+{
+  DynArray<int> dynarray;
+  dynarray.SetEmptyValue(-1);
+
+  cout << "Empty: "; Show(dynarray);
+
+  dynarray[2]=1;
+
+  cout << "2 := 1: "; Show(dynarray);
+
+  dynarray.Append(3);
+
+  cout << "append 3: "; Show(dynarray);
+
+  dynarray[23]=23;
+
+  cout << "23 := 23: "; Show(dynarray);
+}
