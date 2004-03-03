130045adda0a0062765815ffc74c1672613f38af
farin <farin@89ed393f-2632-0410-9d69-91a09fb2310b>
2004-03-03 19:57:43 +0000

    added example code to cvs
    git-svn-id: svn://svn.code.sf.net/p/libvideogfx/code/trunk@128 89ed393f-2632-0410-9d69-91a09fb2310b

---
+++ libvideogfx/examples/symmatrix.cc
@@ -0,0 +1,31 @@
+
+#include "libvideogfx/libvideogfx.hh"
+#include <iostream>
+
+using namespace videogfx;
+using namespace std;
+
+
+void Show(const SymMatrix<int>& d)
+{
+  for (int i=0;i<d.AskSize();i++)
+    {
+      for (int j=0;j<d.AskSize();j++)
+	cout << d.Ask(i,j) << ' ';
+      cout << endl;
+    }
+}
+
+main()
+{
+  SymMatrix<int> m;
+  m.Create(6);
+
+  for (int i=0;i<6;i++)
+    for (int j=0;j<6;j++)
+      {
+	m.Ask(i,j) = i*6+j;
+      }
+
+  Show(m);
+}
