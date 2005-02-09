1b9152f964fbd4a15586af5d5cee734a21283862
farin <farin@89ed393f-2632-0410-9d69-91a09fb2310b>
2005-02-09 19:59:38 +0000

    median test program
    git-svn-id: svn://svn.code.sf.net/p/libvideogfx/code/trunk@160 89ed393f-2632-0410-9d69-91a09fb2310b

---
+++ libvideogfx/examples/median.cc
@@ -0,0 +1,42 @@
+// ./median filename filterradius
+
+#include "libvideogfx/x11/imgwin.hh"
+#include "libvideogfx/graphics/fileio/unified_loader.hh"
+#include "libvideogfx/graphics/filters/median.hh"
+
+#include <unistd.h>
+#include <iostream>
+using namespace videogfx;
+using namespace std;
+
+int main(int argc,char** argv)
+{
+  try
+    {
+      ImageWindow_Autorefresh_X11 win;
+      Image<Pixel> img;
+
+      UnifiedImageLoader reader;
+      reader.SetInput(argv[1]);
+      reader.SetTargetColorspace(Colorspace_Greyscale);
+      reader.ReadImage(img);
+
+      win.Create(img.AskWidth(),img.AskHeight(),"test");
+      win.Display(img);
+
+      Image<Pixel> filtered;
+      filtered = img.Clone();
+
+      while (!reader.IsEOF())
+	{
+	  reader.ReadImage(img);
+	  Median(filtered.AskBitmapY(), img.AskBitmapY(), atoi(argv[2]));
+	  win.Display(filtered);
+	}
+    }
+  catch (const Excpt_Base& e)
+    {
+      MessageDisplay::Show(e);
+    }
+  return 0;
+}
