8ef46241a836e13dc4bc0f69faebc943cdc736cf
farindk <farindk@89ed393f-2632-0410-9d69-91a09fb2310b>
2007-02-06 14:07:31 +0000

    added 4x4 matrix inversion code
    git-svn-id: svn://svn.code.sf.net/p/libvideogfx/code/trunk@185 89ed393f-2632-0410-9d69-91a09fb2310b

--- libvideogfx/libvideogfx/graphics/geometry/matrix.cc
+++ libvideogfx/libvideogfx/graphics/geometry/matrix.cc
@@ -226,7 +226,7 @@ namespace videogfx {
       {
 	/*
 	  3x3 matrix inverse written with cross products:
-	  [ u v w ]^-1 = 1/(u*(vxw)) [ vxw | wxu | uxv ]
+	  [ u v w ]^-1 = 1/(u*(vxw)) [ vxw | wxu | uxv ]^T
 	 */
 	inv[0][0] = +(d_mat[1][1]*d_mat[2][2] - d_mat[1][2]*d_mat[2][1]);
 	inv[0][1] = -(d_mat[2][2]*d_mat[0][1] - d_mat[2][1]*d_mat[0][2]);
