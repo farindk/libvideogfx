f57fc0f858377ccb70f35eafbc4012df599ba894
farindk <farindk@89ed393f-2632-0410-9d69-91a09fb2310b>
2007-06-29 15:20:09 +0000

    sample sequence conversion program
    git-svn-id: svn://svn.code.sf.net/p/libvideogfx/code/trunk@203 89ed393f-2632-0410-9d69-91a09fb2310b

---
+++ libvideogfx/examples/convfile.cc
@@ -0,0 +1,38 @@
+
+#include "libvideogfx/x11/imgwin.hh"
+#include "libvideogfx/graphics/fileio/unified_reader.hh"
+#include "libvideogfx/graphics/fileio/unified_writer.hh"
+
+#include <unistd.h>
+#include <iostream>
+using namespace videogfx;
+using namespace std;
+
+int main(int argc,char** argv)
+{
+  try
+    {
+      Image<Pixel> img;
+
+      UnifiedImageReader reader;
+      reader.SetInput(argv[1]);
+
+      UnifiedImageWriter writer;
+      writer.SetOutput(argv[2]);
+
+      int idx=1;
+      while (!reader.IsEOF())
+	{
+	  reader.ReadImage(img);
+	  writer.WriteImage(img);
+
+	  cout << "frame:" << idx << endl;
+	  idx++;
+	}
+    }
+  catch (const Excpt_Base& e)
+    {
+      MessageDisplay::Show(e);
+    }
+  return 0;
+}
