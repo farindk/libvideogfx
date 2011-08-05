ad03de681be4d6f05c68df52770ca9906e13d397
farindk <farindk@89ed393f-2632-0410-9d69-91a09fb2310b>
2011-08-05 08:49:44 +0000

    API fix for libpng >=1.5 (thanks to Thomas Klausner for providing the patch)
    git-svn-id: svn://svn.code.sf.net/p/libvideogfx/code/trunk@223 89ed393f-2632-0410-9d69-91a09fb2310b

--- libvideogfx/libvideogfx/graphics/fileio/png.cc
+++ libvideogfx/libvideogfx/graphics/fileio/png.cc
@@ -114,7 +114,7 @@ namespace videogfx {
      * the normal method of doing things with libpng).  REQUIRED unless you
      * set up your own error handlers in the png_create_read_struct() earlier.
      */
-    if (setjmp(png_ptr->jmpbuf)) {
+    if (setjmp(png_jmpbuf(png_ptr))) {
       /* Free all of the memory associated with the png_ptr and info_ptr */
       png_destroy_read_struct(&png_ptr, &info_ptr, (png_infopp)NULL);
       /* If we get here, we had a problem reading the file */
@@ -310,7 +310,7 @@ namespace videogfx {
     /* Set error handling.  REQUIRED if you aren't supplying your own
      * error hadnling functions in the png_create_write_struct() call.
      */
-    if (setjmp(png_ptr->jmpbuf)) {
+    if (setjmp(png_jmpbuf(png_ptr))) {
       /* If we get here, we had a problem reading the file */
       png_destroy_write_struct(&png_ptr,  (png_infopp)NULL);
       AssertDescr(false, "could not write png file");
